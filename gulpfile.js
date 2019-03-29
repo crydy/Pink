4ebce27e88e5ef79667df26026948a82ce1a1d90
crydy <13d@ro.ru>
2019-03-29 22:21:29 +0500

    Gulp is redone, some dev-dependencies added

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,89 @@
 const { series, parallel, src, dest, watch } = require('gulp'),
-      less = require('gulp-less'),
-      plumber = require('gulp-plumber'),
-      postcss = require('gulp-postcss'),
-      autoprefixer = require('autoprefixer'),
-      server = require('browser-sync').create();
+      less = require('gulp-less'),                 // препроцессор CSS
+      plumber = require('gulp-plumber'),           // ловец ошибок в watch
+      posthtml = require('gulp-posthtml'),         // подцеплен только для posthtml-include
+      include = require('posthtml-include'),       // используется для интеграции SVG-спрайта
+      postcss = require('gulp-postcss'),           // подцеплен только для autoprefixer
+      autoprefixer = require('autoprefixer'),      // авторасстановка вендерных префиксов
+      minify = require('gulp-csso'),               // минимизатор CSS
+      rename = require('gulp-rename'),             // возможность переименования в Gulp
+      imagemin = require('gulp-imagemin'),         // оптимизатор изображений
+      webp = require('gulp-webp'),                 // создатель webp-копий изображений
+      server = require('browser-sync').create();   // живой сервер
 
-function lessToCss() {
-  console.log('LESS to CSS');
-  return src('src/less/style.less')
-    .pipe(plumber())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(dest('build/css'));
+// Команды консоли
+exports.server = startServer;
+exports.html = copyHTML;
+exports.js = copyJS;
+exports.less = lessToCss;
+exports.css = lessToCss;
+exports.server = startServer;
+exports.img = copyIMG;
+exports.webp = createWEBP;
+
+exports.go = series(startServer, goWatch);
+exports.default = series(startServer, goWatch);
+
+// Консольные сообщения
+const message = {
+  consoleServerStart : function() {
+    console.log('+++++++++++++++++++      Server start      ++++++++++++++++++++');
+  },
+  consoleServerReload : function() {
+    console.log('+++++++++++++++++++      Server reload      +++++++++++++++++++');
+  },
+  consoleCopyHTML : function() {
+    console.log('---                                                         ---');
+    console.log('-      Copy HTML (with <include> integration) >>> build       -');
+    console.log('---                                                         ---');
+  },
+  consoleCopyJS : function() {
+    console.log('---                                                         ---');
+    console.log('-                    Copy JS >>> build/js                     -');
+    console.log('---                                                         ---');
+  },
+  consoleLESStoCSS : function() {
+    console.log('---                                                         ---');
+    console.log('-   LESS to CSS (+ autorpefixing & minifying) >>> build/css   -');
+    console.log('---                                                         ---');
+  },
+  consoleCopyIMG : function() {
+    console.log('---                                                         ---');
+    console.log('-            Copy & optimisation IMG >>> build/img            -');
+    console.log('---                                                         ---');
+  },
+  consoleCreateWEBP : function() {
+    console.log('---                                                         ---');
+    console.log('-    WEBP-copies creation from png/jpg >>> build/img/webp     -');
+    console.log('---                                                         ---');
+  }
+}
+
+
+// ---------------------------------------------------------
+// ------------------------ Функции ------------------------
+// ---------------------------------------------------------
+
+// Слушать файлы
+// (отдельное правило для sprite.svg - он инлайнится в html)
+function goWatch() {
+  // ... подписаться на файлы
+  const watcherHTML = watch(['src/**/*.html']),
+        watcherLESS = watch(['src/less/**/*.less']),
+        watcherJS = watch(['src/js/**/*.js']),
+        watcherIMG = watch(['src/img/**/*.{png,jpg,jpeg,svg}', '!src/img/sprite.svg']),
+        watcherSpriteSVG = watch(['src/img/sprite.svg']);
+  // ... и запустить по событиям файловой системы
+  watcherHTML.on('all', series(copyHTML, serverReload));
+  watcherLESS.on('all', series(lessToCss, serverReload));
+  watcherJS.on('all', series(copyJS, serverReload));
+  watcherIMG.on('all', series(parallel(copyIMG, createWEBP), serverReload));
+  watcherSpriteSVG.on('all', series(copyHTML, serverReload));
 };
 
+// Запуск сервера BrowserSync
 function startServer(cb) {
+  message.consoleServerStart();
   server.init({
     server: {
       baseDir: "./build/"
@@ -25,30 +92,64 @@ function startServer(cb) {
   cb();
 };
 
+// Обновление сервера BrowserSync
 function serverReload(cb) {
-  console.log('server reload');
+  message.consoleServerReload();
   server.reload();
   cb();
 };
 
+// Скопировать HTML в build
+// с интеграцией из тега <include>
 function copyHTML() {
-  console.log('CopyHTML');
+  message.consoleCopyHTML();
   return src('src/*.html')
+    .pipe(posthtml([
+      include()
+    ]))
     .pipe(dest('build'));
 };
 
+// Скопировать JS
 function copyJS() {
-  console.log('CopyJS');
+  message.consoleCopyJS();
   return src('src/js/**/*.js')
     .pipe(dest('build/js'));
 };
 
-// Команды консоли
-exports.less = lessToCss;
-exports.bs = startServer;
-exports.html = copyHTML;
+// Компилировать LESS в CSS
+// с автопрефиксификацией и минификацией
+function lessToCss() {
+  message.consoleLESStoCSS();
+  return src('src/less/style.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(dest('build/css'))
+    .pipe(minify())
+    .pipe(rename('style.min.css'))
+    .pipe(dest('build/css'));
+};
 
-exports.default = startServer;
+// Копирование и оптимизация картинок
+// с исключением для sprite.svg
+function copyIMG() {
+  message.consoleCopyIMG();
+  return src(['src/img/**/*.{png,jpg,jpeg,svg}', '!src/img/sprite.svg'])
+    .pipe(imagemin([
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.svgo()
+    ]))
+    .pipe(dest('build/img'));
+};
 
-// Прослушка событий
-watch(['src/less/**/*.less', 'src/**/*.html', 'src/js/**/*.js'], { events: 'all' }, series(copyHTML, lessToCss, copyJS, serverReload));
+// создать webp-копии растровых картинок
+function createWEBP() {
+  message.consoleCreateWEBP();
+  return src('src/img/**/*.{png,jpg,jpeg}')
+    .pipe(webp({quality: 90}))
+    .pipe(dest('build/img/webp'));
+}
